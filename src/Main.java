75861d00885d451560fc92d22b1cc74fd4309222
Borja de Saa <bdesaavazquez@danielcastelao.org>
2025-05-10 12:05:36 +0200

    Corregida la lógica y test superados

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,73 @@ public class Main {
      * metodo para devolver la puntuacion de tenis
      */
     public static String getScore(int player1Score, int player2Score) {
-        if (player1Score < 0 || player2Score < 0 || player1Score > 4 || player2Score > 4) {
-            return "Invalid score input!";
-        }
-
         String score = "";
+        int tempScore = 0;
 
+        // Caso empate
         if (player1Score == player2Score) {
-            score = getTieScore(player1Score);
+            switch (player1Score) {
+                case 0:
+                    score = "Love-All";
+                    break;
+                case 1:
+                    score = "Fifteen-All";
+                    break;
+                case 2:
+                    score = "Thirty-All";
+                    break;
+                case 3:
+                    score = "Forty-All";
+                    break;
+                default:
+                    score = "Deuce";
+                    break;
+            }
         }
+        // Caso "Advantage" o "Win"
         else if (player1Score >= 4 || player2Score >= 4) {
-            score = getWinnerScore(player1Score, player2Score);
+            int scoreDifference = player1Score - player2Score;
+
+            // Asegurarse de que la diferencia es 1 o mayor para "Advantage"
+            if (scoreDifference == 1) {
+                score = "Advantage player1";
+            } else if (scoreDifference == -1) {
+                score = "Advantage player2";
+            } else if (scoreDifference >= 2) {
+                score = "Win for player1";
+            } else {
+                score = "Win for player2";
+            }
         }
+        // Caso normal
         else {
-            score = getNormalScore(player1Score, player2Score);
+            for (int i = 1; i < 3; i++) {
+                if (i == 1) tempScore = player1Score;
+                else {
+                    score += "-";
+                    tempScore = player2Score;
+                }
+
+                switch (tempScore) {
+                    case 0:
+                        score += "Love";
+                        break;
+                    case 1:
+                        score += "Fifteen";
+                        break;
+                    case 2:
+                        score += "Thirty";
+                        break;
+                    case 3:
+                        score += "Forty";
+                        break;
+                }
+            }
         }
 
         return score;
     }
+
 
     /**
      * Método para obtener la puntuación cuando hay un empate.
