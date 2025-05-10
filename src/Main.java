7c7d5cd8e6448fdf937c743f3c37ce38ceefb57e
Borja de Saa <bdesaavazquez@danielcastelao.org>
2025-05-10 11:54:37 +0200

    Extracción de métodos para manejar empate y puntuación normal

--- src/Main.java
+++ src/Main.java
@@ -3,75 +3,79 @@ public class Main {
      * metodo para devolver la puntuacion de tenis
      */
 
-    public static String getScore(int player1Score, int player2Score) {
-        String score = "";
-        int tempScore = 0;
+        public static String getScore(int scorePlayer1, int scorePlayer2) {
+            String score = "";
 
-        if (player1Score == player2Score) {
-            score = getTieScore(player1Score);
+            // Si los puntajes son iguales
+            if (scorePlayer1 == scorePlayer2) {
+                score = getTieScore(scorePlayer1);
+            }
+            // Si un jugador tiene más de 4 puntos
+            else if (scorePlayer1 >= 4 || scorePlayer2 >= 4) {
+                int minusResult = scorePlayer1 - scorePlayer2;
+                if (minusResult == 1) score = "Advantage player1";
+                else if (minusResult == -1) score = "Advantage player2";
+                else if (minusResult >= 2) score = "Win for player1";
+                else score = "Win for player2";
+            }
+            // Si los puntajes no son iguales y ninguno tiene más de 4 puntos
+            else {
+                score = getNormalScore(scorePlayer1, scorePlayer2);
+            }
+
+            return score;
         }
-        else if (player1Score >= 4 || player2Score >= 4) {
-            score = getAdvantageOrWin(player1Score, player2Score);
-        }
-        else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = player1Score;
-                else {
-                    score += "-";
-                    tempScore = player2Score;
-                }
-                score += getScoreForTemp(tempScore);
+
+        /**
+         * Devuelve la puntuación de empate
+         */
+        private static String getTieScore(int score) {
+            switch (score) {
+                case 0:
+                    return "Love-All";
+                case 1:
+                    return "Fifteen-All";
+                case 2:
+                    return "Thirty-All";
+                case 3:
+                    return "Forty-All";
+                default:
+                    return "Deuce";
             }
         }
-        return score;
-    }
 
-     //Método para manejar el caso de empate
-
-    private static String getTieScore(int score) {
-        switch (score) {
-            case 0:
-                return "Love-All";
-            case 1:
-                return "Fifteen-All";
-            case 2:
-                return "Thirty-All";
-            case 3:
-                return "Forty-All";
-            default:
-                return "Deuce";
+        /**
+         * Devuelve la puntuación normal para los casos que no son empate
+         */
+        private static String getNormalScore(int scorePlayer1, int scorePlayer2) {
+            String score = "";
+            int tempScore = 0;
+            for (int i = 1; i < 3; i++) {
+                if (i == 1) tempScore = scorePlayer1;
+                else {
+                    score += "-";
+                    tempScore = scorePlayer2;
+                }
+                score += getScoreName(tempScore);
+            }
+            return score;
         }
-    }
 
-     // Método para manejar los casos de puntuación en ventaja o victoria
-
-    private static String getAdvantageOrWin(int player1Score, int player2Score) {
-        int scoreDifference = player1Score - player2Score;
-        if (scoreDifference == 1) {
-            return "Advantage player1";
-        } else if (scoreDifference == -1) {
-            return "Advantage player2";
-        } else if (scoreDifference >= 2) {
-            return "Win for player1";
-        } else {
-            return "Win for player2";
+        /**
+         * Devuelve el nombre de la puntuación
+         */
+        private static String getScoreName(int score) {
+            switch (score) {
+                case 0:
+                    return "Love";
+                case 1:
+                    return "Fifteen";
+                case 2:
+                    return "Thirty";
+                case 3:
+                    return "Forty";
+                default:
+                    return "";
+            }
         }
-    }
-
-     // Método para devolver la puntuación en formato texto
-
-    private static String getScoreForTemp(int score) {
-        switch (score) {
-            case 0:
-                return "Love";
-            case 1:
-                return "Fifteen";
-            case 2:
-                return "Thirty";
-            case 3:
-                return "Forty";
-            default:
-                return "";
-        }
-    }
     }
